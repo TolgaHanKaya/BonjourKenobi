6977ecb2276a9062b8af52c7cf18ac322d32d69f
Tolga <tlghankaya@gmail.com>
2020-07-22 04:26:12 +0300

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Tolga Han Kaya on 22.07.2020.
+//  Copyright © 2020 TolgaHanKaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
